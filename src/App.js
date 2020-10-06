7ce2c03a6fb6d9670c02701c9337ca1ab4d6c17d
Fred OLIVE <frol.developpement@gmail.com>
2020-10-06 21:09:09 +0200

    create card component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css'
 
 import Header from './components/Header'
 import Admin from './components/Admin'
+import Card from './components/Card'
 import recettes from './recettes'
 
 class App extends Component {
@@ -15,13 +16,13 @@ class App extends Component {
   chargerExemple = () => this.setState({recettes})
 
   render () {
+    const cards = Object.keys(this.state.recettes)
+    .map(key => <Card key={key} details={this.state.recettes[key]} />)
     return (
       <div className='box'>
         <Header pseudo={this.state.pseudo} />
         <div className='cards'>
-          <div className='card'>
-            <h2>Une Carte</h2>
-          </div>
+          {cards}
         </div>
         <Admin
         chargerExemple={this.chargerExemple}/>
